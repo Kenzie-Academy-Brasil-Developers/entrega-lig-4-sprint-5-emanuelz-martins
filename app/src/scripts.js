9fc0d99c71a424d4751011c4f6c01985ddff2c1d
emanuelz-martins <emanuelzm@protonmail.com>
2021-08-10 15:20:38 -0300

    order correction

--- app/src/scripts.js
+++ app/src/scripts.js
@@ -1,29 +1,30 @@
 const mainTable = document.querySelector('#main-container');
 const mapArray = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
 ];
 
 let diskCount = 0;
 let match = false;
 
 function createTable() {
-    for (let row = 0; row < mapArray.length; row++) {
-        const tableRow = document.createElement('div');
-        tableRow.classList.add('row');
+    for (let column = 0; column < mapArray.length; column++) {
+        const tableColumn = document.createElement('div');
+        tableColumn.classList.add('column');      
         
-        for (let column = 0; column < mapArray[row].length; column++) {
-            const tableColumn = document.createElement('div');
-            tableColumn.classList.add('column');      
-            
-            tableRow.appendChild(tableColumn);
+        for (let row = 0; row < mapArray[column].length; row++) {
+            const tableRow = document.createElement('div');
+            tableRow.classList.add('row');
+
+            tableColumn.appendChild(tableRow);
         }
 
-        mainTable.appendChild(tableRow);
+        mainTable.appendChild(tableColumn);
     }
 }
 
